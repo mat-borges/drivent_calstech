437ed84ddd341d098af222f91f45114a2eeb1c67
Mateus Borges <borgesm.work@gmail.com>
2023-01-23 10:26:04 -0300

    style: formatter on controller

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -1,7 +1,7 @@
-import { AuthenticatedRequest } from '@/middlewares';
-import { Response } from 'express';
-import enrollmentsService from '@/services/enrollments-service';
-import httpStatus from 'http-status';
+import { AuthenticatedRequest } from "@/middlewares";
+import { Response } from "express";
+import enrollmentsService from "@/services/enrollments-service";
+import httpStatus from "http-status";
 
 export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
@@ -35,7 +35,7 @@ export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response
     const address = await enrollmentsService.getAddressFromCEP(cep);
     res.status(httpStatus.OK).send(address);
   } catch (error) {
-    if (error.name === 'NotFoundError') {
+    if (error.name === "NotFoundError") {
       return res.send(httpStatus.NO_CONTENT);
     }
   }
