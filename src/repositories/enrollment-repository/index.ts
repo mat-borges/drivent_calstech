3600de61405be4731041fb50577802d7399b75e2
Mateus Borges <borgesm.work@gmail.com>
2023-01-23 10:26:51 -0300

    style: formatter on enrollment-repository

--- src/repositories/enrollment-repository/index.ts
+++ src/repositories/enrollment-repository/index.ts
@@ -1,5 +1,5 @@
-import { prisma } from "@/config";
 import { Enrollment } from "@prisma/client";
+import { prisma } from "@/config";
 
 async function findWithAddressByUserId(userId: number) {
   return prisma.enrollment.findFirst({
