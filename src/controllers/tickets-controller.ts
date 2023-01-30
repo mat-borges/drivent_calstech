5b09b88d914d3f56ea19c80b32d1f7b17caa64cc
Mateus Borges <borgesm.work@gmail.com>
2023-01-30 11:47:00 -0300

    feat: createUserTicket, refactor: adapt to match repository new return

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -15,13 +15,31 @@ export async function getTicketsTypes(req: AuthenticatedRequest, res: Response) 
 export async function getUserTickets(req: AuthenticatedRequest, res: Response) {
   try {
     const userTickets = await ticketsService.getUserTickets(req.userId);
-    if (userTickets.length === 0) {
+
+    if (!userTickets) {
       return res.sendStatus(httpStatus.NOT_FOUND);
     } else {
       return res.status(httpStatus.OK).send(userTickets);
     }
   } catch (error) {
     if (error.name === "NotDoundError") {
+      return res.sendStatus(httpStatus.NOT_FOUND);
+    } else {
+      return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({});
+    }
+  }
+}
+
+export async function createUserTicket(req: AuthenticatedRequest, res: Response) {
+  const { ticketTypeId } = req.body as { ticketTypeId: number };
+
+  if (!ticketTypeId) return res.sendStatus(httpStatus.BAD_REQUEST);
+
+  try {
+    const ticket = await ticketsService.createUserTicket(req.userId, ticketTypeId);
+    res.status(httpStatus.CREATED).send(ticket);
+  } catch (error) {
+    if (error.name === "NotFoundError") {
       return res.sendStatus(httpStatus.NOT_FOUND);
     } else {
       return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({});
