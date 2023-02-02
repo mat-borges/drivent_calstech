24ceee96a644af0ac0400a54eb86c50dc0b29d3b
Mateus Borges <borgesm.work@gmail.com>
2023-02-02 16:43:55 -0300

    refactor: change imports

--- src/routers/payments-router.ts
+++ src/routers/payments-router.ts
@@ -1,5 +1,5 @@
 import { authenticateToken, validateBody } from "@/middlewares";
-import { getTicketPayment, postTicketPayment } from "@/controllers/payments-controller";
+import { getTicketPayment, postTicketPayment } from "@/controllers";
 
 import { Router } from "express";
 import { paymentSchema } from "@/schemas/payments-schema";
--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -1,5 +1,5 @@
 import { authenticateToken, validateBody } from "@/middlewares";
-import { createUserTicket, getTicketsTypes, getUserTickets } from "@/controllers/tickets-controller";
+import { createUserTicket, getTicketsTypes, getUserTickets } from "@/controllers";
 
 import { Router } from "express";
 import { ticketSchema } from "@/schemas/tickets-schema";
@@ -7,7 +7,7 @@ import { ticketSchema } from "@/schemas/tickets-schema";
 const ticketsRouter = Router();
 
 ticketsRouter
-  .use("/*", authenticateToken)
+  .all("/*", authenticateToken)
   .get("/types", getTicketsTypes)
   .get("/", getUserTickets)
   .post("/", validateBody(ticketSchema), createUserTicket);
