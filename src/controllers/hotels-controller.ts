2be9d7a3df85c0db9da7d4a08a4021a64faddcb5
Mateus Borges <borgesm.work@gmail.com>
2023-02-06 14:50:21 -0300

    refactor: function to check enrollment and ticket type

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -1,14 +1,14 @@
 import { AuthenticatedRequest } from "@/middlewares";
 import { Response } from "express";
-import { badRequestError } from "@/errors/bad-request-error";
 import hotelsService from "@/services/hotels-service";
 import httpStatus from "http-status";
 
 export async function getHotels(req: AuthenticatedRequest, res: Response) {
   const userId = req.userId;
+  await hotelsService.checkEnrollmentAndTicketType(userId);
 
   try {
-    const hotels = await hotelsService.getHotels(userId);
+    const hotels = await hotelsService.getHotels();
     res.status(httpStatus.OK).send(hotels);
   } catch (error) {
     if (error.name === "NotFoundError") {
@@ -24,9 +24,10 @@ export async function getHotels(req: AuthenticatedRequest, res: Response) {
 export async function getHotelRooms(req: AuthenticatedRequest, res: Response) {
   const hotelId = parseInt(req.params.hotelId);
   const userId = req.userId;
+  await hotelsService.checkEnrollmentAndTicketType(userId);
 
   try {
-    const rooms = await hotelsService.getHotelRooms(hotelId, userId);
+    const rooms = await hotelsService.getHotelRooms(hotelId);
     res.status(httpStatus.OK).send(rooms);
   } catch (error) {
     if (error.name === "NotFoundError") {
