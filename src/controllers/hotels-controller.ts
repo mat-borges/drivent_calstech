70efc6247305247f92694b8ee3c619ffd985eaa4
Mateus Borges <borgesm.work@gmail.com>
2023-02-06 12:19:43 -0300

    refactor: remove bad request error from /hotels

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -7,8 +7,6 @@ import httpStatus from "http-status";
 export async function getHotels(req: AuthenticatedRequest, res: Response) {
   const userId = req.userId;
 
-  if (!userId) throw badRequestError();
-
   try {
     const hotels = await hotelsService.getHotels(userId);
     res.status(httpStatus.OK).send(hotels);
@@ -17,8 +15,6 @@ export async function getHotels(req: AuthenticatedRequest, res: Response) {
       return res.status(httpStatus.NOT_FOUND).send(error.message);
     } else if (error.name === "PaymentRequired") {
       return res.status(httpStatus.PAYMENT_REQUIRED).send(error.message);
-    } else if (error.name === "BadRequestError") {
-      return res.status(httpStatus.BAD_REQUEST).send(error.message);
     } else {
       return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -29,8 +25,6 @@ export async function getHotelRooms(req: AuthenticatedRequest, res: Response) {
   const hotelId = parseInt(req.params.hotelId);
   const userId = req.userId;
 
-  if (!hotelId || !userId) throw badRequestError();
-
   try {
     const rooms = await hotelsService.getHotelRooms(hotelId, userId);
     res.status(httpStatus.OK).send(rooms);
@@ -39,8 +33,6 @@ export async function getHotelRooms(req: AuthenticatedRequest, res: Response) {
       return res.status(httpStatus.NOT_FOUND).send(error.message);
     } else if (error.name === "PaymentRequired") {
       return res.status(httpStatus.PAYMENT_REQUIRED).send(error.message);
-    } else if (error.name === "BadRequestError") {
-      return res.status(httpStatus.BAD_REQUEST).send(error.message);
     } else {
       return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
     }
