d4d2d7494bff2cebfed3bdbdb8cb76a2e5141efc
Mateus Borges <borgesm.work@gmail.com>
2023-02-06 11:27:26 -0300

    feat: GET /hotels/:hotelId (hotel rooms), refactor: check enrollment and ticket type function

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -1,10 +1,13 @@
 import { AuthenticatedRequest } from "@/middlewares";
 import { Response } from "express";
+import { badRequestError } from "@/errors/bad-request-error";
 import hotelsService from "@/services/hotels-service";
 import httpStatus from "http-status";
 
 export async function getHotels(req: AuthenticatedRequest, res: Response) {
   const userId = req.userId;
+
+  if (!userId) throw badRequestError();
 
   try {
     const hotels = await hotelsService.getHotels(userId);
@@ -14,12 +17,32 @@ export async function getHotels(req: AuthenticatedRequest, res: Response) {
       return res.status(httpStatus.NOT_FOUND).send(error.message);
     } else if (error.name === "PaymentRequired") {
       return res.status(httpStatus.PAYMENT_REQUIRED).send(error.message);
+    } else if (error.name === "BadRequestError") {
+      return res.status(httpStatus.BAD_REQUEST).send(error.message);
     } else {
-      return res.sendStatus(httpStatus.BAD_REQUEST);
+      return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
 
 export async function getHotelRooms(req: AuthenticatedRequest, res: Response) {
-  res.sendStatus(httpStatus.NOT_IMPLEMENTED);
+  const hotelId = parseInt(req.params.hotelId);
+  const userId = req.userId;
+
+  if (!hotelId || !userId) throw badRequestError();
+
+  try {
+    const rooms = await hotelsService.getHotelRooms(hotelId, userId);
+    res.status(httpStatus.OK).send(rooms);
+  } catch (error) {
+    if (error.name === "NotFoundError") {
+      return res.status(httpStatus.NOT_FOUND).send(error.message);
+    } else if (error.name === "PaymentRequired") {
+      return res.status(httpStatus.PAYMENT_REQUIRED).send(error.message);
+    } else if (error.name === "BadRequestError") {
+      return res.status(httpStatus.BAD_REQUEST).send(error.message);
+    } else {
+      return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -3,7 +3,7 @@ import hotelRepository from "@/repositories/hotels-repository";
 import { notFoundError } from "@/errors";
 import ticketsRepository from "@/repositories/tickets-repository";
 
-async function getHotels(userId: number) {
+async function checkEnrollmentAndTicketType(userId: number) {
   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
   if (!enrollment) throw notFoundError();
 
@@ -14,15 +14,33 @@ async function getHotels(userId: number) {
   if (ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
     throw { name: "PaymentRequired", message: "Your ticket doesn't include hotel." };
   }
+}
+
+async function getHotels(userId: number) {
+  checkEnrollmentAndTicketType(userId);
 
   const hotels = await hotelRepository.getHotels();
-  if (!hotels || hotels.length === 0) throw notFoundError();
+  if (!hotels || hotels.length === 0) {
+    throw notFoundError();
+  } else {
+    return hotels;
+  }
+}
 
-  return hotels;
+async function getHotelRooms(hotelId: number, userId: number) {
+  checkEnrollmentAndTicketType(userId);
+
+  const rooms = await hotelRepository.getHotelRooms(hotelId);
+  if (!rooms || rooms.Rooms.length === 0) {
+    throw notFoundError();
+  } else {
+    return rooms;
+  }
 }
 
 const hotelsService = {
   getHotels,
+  getHotelRooms,
 };
 
 export default hotelsService;
